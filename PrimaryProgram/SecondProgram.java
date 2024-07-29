32acfb4ad59fbbfa265982279a5315c6d32501c7
drohit02 <drohit045@gmail.com>
2024-07-29 14:34:05 +0530

    second basic program

---
+++ PrimaryProgram/SecondProgram.java
@@ -0,0 +1,12 @@
+package com.PrimaryProgram;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+
+	}
+
+}
